67cce0ab1c64efcc436081227d0fb00cf9045c9c
simonpatrick <simonpatrick@163.com>
2015-03-09 00:53:03 +0800

    init projects

---
+++ git_init.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+remote_url=$1
+echo $remote_url
+comments=$2
+echo $comments
+
+git remote add origin git@github.com:simonpatrick/$remote_url.git 
+git add .
+git commit . -m "$comments"
+git push -u origin master
