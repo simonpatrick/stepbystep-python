9921cfc64c1021c006bbc866b947c2f97ba38d6c
simonpatrick <simonpatrick@163.com>
2015-05-15 17:56:06 +0800

    add selenium scripts

---
+++ webdriver_guide/seleniums/pageobjects/pages/homepage.py
@@ -0,0 +1,19 @@
+# _*_ coding=utf-8 _*_
+__author__ = 'patrick'
+from basepage import BasePage
+from basepage import InvalidPageException
+
+
+class HomePage(BasePage):
+
+    _home_page_slideshow_locator = 'slideshow-container'
+
+    def __init__(self, driver):
+        super(HomePage, self).__init__(driver)
+
+    def _validate_page(self, driver):
+        try:
+            driver.\
+                find_element_by_class_name(self._home_page_slideshow_locator)
+        except:
+            raise InvalidPageException("Home Page not loaded")
