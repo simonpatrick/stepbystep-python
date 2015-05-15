a0d03348f79d26cfef01535164aada917512d6d3
simonpatrick <simonpatrick@163.com>
2015-05-15 17:01:45 +0800

    add simple tests

---
+++ testing/simpletest.py
@@ -0,0 +1,36 @@
+# _*_ coding=utf-8 _*_
+import unittest
+__author__ = 'patrick'
+
+
+class MyTests(unittest.TestCase):
+    def test_average(self):
+        print "my Tests"
+        pass
+
+class MyTest2(unittest.TestCase):
+    def test_another_test(self):
+        print "My Test2"
+        pass
+
+def test_suite():
+    def _suite(test_class):
+        return unittest.makeSuite(test_class)
+
+    suite=unittest.TestSuite()
+    suite.addTest((_suite(MyTests),_suite(MyTest2)))
+    return suite
+
+unittest.main(defaultTest='test_suite')
+
+if __name__ == '__main__':
+    unittest.main(defaultTest='test_suite')
+
+'''
+def setup():
+    pass
+
+def teardown():
+    pass
+
+'''
