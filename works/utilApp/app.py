bea3af690a6b3d114e37d62509fc476c3b6bd69d
patrick <patrick@patrickdeMacBook-Pro.local>
2015-10-25 16:26:54 +0800

    remove name

--- works/utilApp/app.py
+++ works/utilApp/app.py
@@ -1,5 +1,5 @@
 # _*_ coding=utf-8 _*_
-import re
+import json
 import requests
 
 __author__ = 'patrick'
@@ -8,30 +8,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-username = "110863"
-password = "********"
+username = "******"
+password = "******"
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @app.route("/getPassword")
 def get_integration_password():
     s = requests.Session()
-    login_url = "http://login.dooioo.org/login?usercode=%s&password=%s" % (username, password)
+    login_url = "http://login.****.org/login?usercode=%s&password=%s&companyId=1&encrypt=false" % (username, password)
     r = s.post(login_url, verify=False)
     get_password_url = "https://login.dooioo.org/rootPassword/get"
     login_cookie = r.cookies
     r = s.get(get_password_url, cookies=login_cookie, verify=False)
-    pattern = re.compile(r'【(.*)】')
-    match = pattern.findall(r.content)
-    if(len(match)<=0):return "获取万能密码失败。。。。。。。"
-    return match[0]
+    data = json.dumps(r.content)
+    return data['password']
+
 
 @app.route("/setUserCode/<user_code>")
 def set_user_code(user_code):
     username = user_code
-    return username
+    return "set user code successfully "
 
 
 @app.route("/setpw/<pwd>")
@@ -39,11 +40,13 @@ def set_password(pwd):
     password = pwd
     return password
 
+
 @app.route("/init/usercode/<user_code>/password/<pwd>")
-def init(user_code,pwd):
-    username=user_code
-    password=pwd
-    return username+":"+password
+def init(user_code, pwd):
+    username = user_code
+    password = pwd
+    return username + ":" + password
+
 
 if __name__ == '__main__':
     app.run(debug=True)
