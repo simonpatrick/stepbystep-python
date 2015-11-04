384dc07a5c4177e0ea6cf9c5c0c40d95e247e41a
patrick <patrick@patrickdeMacBook-Pro.local>
2015-11-04 11:00:16 +0800

    update to latest

---
+++ algorithm/common-algorithm/generator.py
@@ -0,0 +1,15 @@
+__author__ = 'patrick'
+def fib(n):
+    """
+    Generator for Fibonacci serie
+
+    Example: for i in fib(5): print i
+    @param n fib range upper bound
+    """
+    a, b = 0, 1
+    i = 0
+    while i < n:
+        yield b
+        a, b = b, a+b
+        i += 1
+
---
+++ algorithm/readme.md
@@ -0,0 +1,5 @@
+# Repo
+
+## From
+
+- [qiwsir](https://github.com/qiwsir/algorithm.git)
